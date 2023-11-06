244f0dfa3064432fb2e22508776c588acd73551b
suzu6633 <Sufiore.6633@gmail.com>
2023-11-06 12:09:20 +0900

    ユーザー編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'messages/index'
-  # Defines the root path route ("/")
-  # root "articles#index"
   root to: "messages#index"
+  resources :users, only: [:edit, :update]
 end
